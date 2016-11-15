dcbb0a8da698efde7a1212d2a0755652567ec77b
tsvetkovpro <285019@gmail.com>
2016-11-15 15:03:18 +0500

    reducers and actions

--- src/app.js
+++ src/app.js
@@ -1,1 +1,40 @@
-console.log('123-321');
+
+
+
+const store = Redux.createStore(function (state, action) {
+	switch (action.type) {
+		case 'ADD_CARD':
+			let newCard = Object.assign({}, action.data, {
+				score: 1,
+				id: +new Date
+			});
+
+			return Object.assign({}, state, {
+				cards: state.cards ? state.cards.concat([newCard]) : [newCard]
+			});
+		default:
+			return state || {};
+	}
+});
+
+
+store.subscribe(() => {
+	console.log(store.getState());
+});
+
+
+store.dispatch({
+	type: 'ADD_CARD',
+	data: {}
+});
+
+
+
+
+
+
+
+
+
+
+
