d28bc9f0701a23550e363e22005415a6ece04b75
tsvetkovpro <285019@gmail.com>
2016-11-15 15:41:31 +0500

    combining reducers

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 
 
-
-const store = Redux.createStore(function (state, action) {
+const cards = (state, action) => {
 	switch (action.type) {
 		case 'ADD_CARD':
 			let newCard = Object.assign({}, action.data, {
@@ -9,17 +8,29 @@ const store = Redux.createStore(function (state, action) {
 				id: +new Date
 			});
 
-			return Object.assign({}, state, {
-				cards: state.cards ? state.cards.concat([newCard]) : [newCard]
-			});
+			return state.concat([newCard]);
+
 		default:
-			return state || {};
+			return state || [];
 	}
-});
+};
+
+const store = Redux.createStore(Redux.combineReducers({
+	cards
+}));
 
 
 store.subscribe(() => {
 	console.log(store.getState());
+});
+
+
+store.dispatch({
+	type: 'ADD_CARD',
+	data: {
+		main: 'main',
+		my: 'my'
+	}
 });
 
 
