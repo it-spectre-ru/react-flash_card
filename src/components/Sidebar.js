c52e90e628c000f4e6a849fc7a13b488671ce65f
tsvetkovpro <285019@gmail.com>
2016-11-19 12:10:35 +0500

    using the `react-redux` package

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { connect } from 'react-redux';
+import {addDeck, showAddDeck, hideAddDeck} from '../actions';
 
-const Sidebar = React.createClass({
+
+const mapStateToProps = ( { decks, addingDeck } ) => ({
+	decks,
+	addingDeck
+});
+
+
+const mapDispatchToProps = dispatch => ({
+	addDeck: name => dispatch(addDeck(name)),
+	showAddDeck: () => dispatch(showAddDeck()),
+	hideAddDeck: () => dispatch(hideAddDeck())
+});
+
+const Sidebar = React.createClass( {
 	componentDidUpdate() {
-		var el = ReactDOM.findDOMNode(this.refs.add);
-		if (el) el.focus();
+		var el = ReactDOM.findDOMNode( this.refs.add );
+		if ( el ) el.focus();
 	},
 	render() {
 		let props = this.props;
@@ -14,20 +29,22 @@ const Sidebar = React.createClass({
 				<h2>All decks</h2>
 				<button onClick={e => this.props.showAddDeck()}>New Deck</button>
 				<ul>
-					{props.decks.map((deck, i) =>
+					{props.decks.map( ( deck, i ) =>
 						<li key={i}> {deck.name} </li>
 					)}
 				</ul>
-				{ props.addingDeck && <input ref='add' onKeyPress={this.createDeck} /> }
+				{ props.addingDeck && <input ref='add' onKeyPress={this.createDeck}/> }
 			</div>
 		);
 	},
-	createDeck(evt) {
-		if (evt.which !== 13) {return}
-		var name = ReactDOM.findDOMNode(this.refs.add).value;
-		this.props.addDeck(name);
+	createDeck( evt ) {
+		if ( evt.which !== 13 ) {
+			return
+		}
+		var name = ReactDOM.findDOMNode( this.refs.add ).value;
+		this.props.addDeck( name );
 		this.props.hideAddDeck();
 	}
-});
+} );
 
-export default Sidebar;
+export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
