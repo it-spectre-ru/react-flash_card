6f37dd2c6b6d9fcfa875e2877bd915983aae471c
tsvetkovpro <285019@gmail.com>
2016-11-19 12:33:54 +0500

    add a router

--- src/app.js
+++ src/app.js
@@ -2,21 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 import * as reducers from './reducers';
+reducers.routing = routerReducer;
+
 import App from './components/App';
-import Sidebar from './components/Sidebar';
 
 const store = createStore( combineReducers( reducers ) );
+const history = syncHistoryWithStore(browserHistory, store);
 
+// const routes = (<Route path='/' component={App}></Route>); --> and use jsx {routes}
+// Fix - Warning: [react-router] You cannot change <Router routes>; it will be ignored
 
 function run() {
 	let state = store.getState();
 	console.log( state );
 	ReactDOM.render(
 		<Provider store={store}>
-			<App>
-				<Sidebar />
-			</App>
+			<Router history={history}>
+				<Route path='/' component={App}></Route>
+			</Router>
 		</Provider>, document.getElementById( 'root' ) );
 }
 
